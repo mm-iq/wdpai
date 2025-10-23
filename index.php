3284151f64e0ef4ff3b48ad045d43a6ea0c95db1
Maksymilian Łyko <nie@ma.emaila>
2025-10-23 17:21:04 +0200

    Add: views, styles, main.js

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
 echo "<h1>Hi Students! 🌅</h1>";
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+//var_dump($path);
+
+switch($path) {
+    case 'dashboard':
+        include 'public/views/dashboard.html';
+        break;
+    case 'login':
+        include 'public/views/login.html';
+        break;
+    default:
+        include 'public/views/404.html';
+        break;
+}
+?>
---
+++ public/scripts/main.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('#tickle');
+console.log(header);
+
+header.addEventListener('click', () => {
+    header.style.color = 'green';
+})
